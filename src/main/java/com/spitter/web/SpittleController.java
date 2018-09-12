47a7528dbd9f327f9b01bab02e268b7701cafb0c
esethuraman <elattp@gmail.com>
2018-09-12 18:29:31 -0400

    path variable support added

--- src/main/java/com/spitter/web/SpittleController.java
+++ src/main/java/com/spitter/web/SpittleController.java
@@ -4,6 +4,7 @@ import com.spitter.app.SpittleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -52,5 +53,14 @@ public class SpittleController {
 
         model.addAttribute(spittleRepository.findSpittles(max, count));
         return "spittles";
+    }
+
+    // ****  PATH VARIABLE ***
+    @RequestMapping(value="/{spittleId}", method=RequestMethod.GET)
+    public String spittle(
+            @PathVariable("spittleId") long spittleId,
+            Model model){
+        model.addAttribute(spittleRepository.findOne(spittleId));
+        return "spittle";
     }
 }
